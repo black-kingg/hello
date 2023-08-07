a1c682ad743199fbb559609830a6d9c16589ef39
Odetunde Mubarak <mudet04@gmail.com>
2023-08-07 12:31:02 +0100

    #

--- src/Pages/Definition.js
+++ src/Pages/Definition.js
@@ -1,31 +1,77 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import NotFound from "../Components/NotFound";
+import DefinitionSearch from "../Components/DefinitionSearch";
 
 export default function Definition() {
   const [word, setWord] = useState();
+  const [notFound, setNotFound] = useState(false);
   let { search } = useParams();
+  const navigate = useNavigate();
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + search)
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status === 404) {
+          setNotFound(true);
+        } else if (response.status === 401) {
+          navigate("/login");
+        } else if (response.status === 500) {
+          setError(true);
+        }
+
+        if (!response.ok) {
+          setError(true);
+
+          throw new Error("Something went wrong");
+        }
+        return response.json();
+      })
       .then((data) => {
         setWord(data[0].meanings);
+      })
+      .catch((e) => {
+        console.log(e.message);
       });
   }, []);
+
+  if (notFound === true) {
+    return (
+      <>
+        <NotFound />
+        <Link to="/dictionary">Search Another</Link>
+      </>
+    );
+  }
+
+  if (error === true) {
+    return (
+      <>
+        <p>Something went wrong</p>
+        <Link to="/dictionary">Search Another</Link>
+      </>
+    );
+  }
+
   return (
     <>
-      <h1>This is a definition:</h1>
-      {word
-        ? word.map((meaning) => {
+      {word ? (
+        <>
+          <h1>This is a definition:</h1>
+          {word.map((meaning) => {
             return (
               <p key={uuidv4()}>
                 {meaning.partOfSpeech + ": "}
                 {meaning.definitions[0].definition}
               </p>
             );
-          })
-        : null}
+          })}
+          <p>Search again:</p>
+          <DefinitionSearch />
+        </>
+      ) : null}
     </>
   );
 }
--- src/Pages/Dictionary.js
+++ src/Pages/Dictionary.js
@@ -1,27 +1,5 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import DefinitionSearch from "../Components/DefinitionSearch";
 
 export default function Dictionary() {
-  const [word, setWord] = useState("");
-  const navigate = useNavigate();
-
-  return (
-    <>
-      <input
-        type="text"
-        onChange={(e) => {
-          setWord(e.target.value);
-        }}
-      />
-      <button
-        className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
-        onClick={() => {
-          navigate("/definition/" + word);
-        }}
-      >
-        {" "}
-        Search{" "}
-      </button>
-    </>
-  );
+  return <DefinitionSearch />;
 }
